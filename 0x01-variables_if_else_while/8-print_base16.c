42b5694002b0d640ad9d4c824d242b0aa778145d
jessica <bocajessica@gmail.com>
2022-09-11 00:40:07 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: (0) sucess
+ */
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+	putchar(c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
