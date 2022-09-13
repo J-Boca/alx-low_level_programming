7844b0893aa2033cc45dfb1260706cde6c5f544f
jessica <bocajessica@gmail.com>
2022-09-13 12:46:06 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - 10 times the alphabet
+ *
+ * Return: (0) sucess
+ */
+
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+{
+	for (b = 97; b <= 122; b++)
+{
+	_putchar(b);
+}
+_putchar('\n');
+}
+}
